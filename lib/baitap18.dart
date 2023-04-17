933bb9cccea56e1ca37470c1eb29b06512adc440
CUSC <fdcvlinh@gmail.com>
2023-04-17 20:28:18 +0700

    17042023

--- lib/baitap18.dart
+++ lib/baitap18.dart
@@ -8,7 +8,7 @@ import 'dart:math';
 void main() {
   int n = InputNumber();
   String result = ListSoNguyenTo(n);
-  print("$n số nguyên tố đầu tiên là: $result");
+  print("$n số nguyên tố đầu tien: $result");
 }
 
 /**
@@ -16,20 +16,24 @@ void main() {
  */
 int InputNumber() {
   int result = 0;
-  stdout.write("Nhập số dương n: ");
+  stdout.write("Nhập số dương cần kiểm tra: ");
   result = int.parse(stdin.readLineSync()!);
   return result;
 }
 
 /**
- * Hàm ListSoNguyenTo(n)
+ * Hàm ListSoNguyenTo
  */
 String ListSoNguyenTo(n) {
   String result = "";
-  for (int i = 2; i <= n; i++) {
+  int count = 0;
+  int i = 2;
+  while (count < n) {
     if (SoNguyenTo(i)) {
       result += "," + i.toString();
+      count++;
     }
+    i++;
   }
   return result.substring(1);
 }
