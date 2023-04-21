71223384448d8979cab10d79b365cdd129bc0561
CUSC <fdcvlinh6@gmail.com>
2023-04-21 20:16:20 +0700

    21042023

--- lib/baitap51.dart
+++ lib/baitap51.dart
@@ -24,7 +24,6 @@ void main() {
 String GiaiPhuongTrinh() {
   var result = [];
   int max = 0;
-  int count = 0;
   for (int x = 0; x <= 200; x++) {
     for (int y = 0; y <= 100; y++) {
       for (int z = 0; z <= 40; z++) {
